5886c5362f81be5f10f41c164f30f42c4e53fd51
Jason Park <stiger3820@gmail.com>
2022-06-24 09:28:45 +0900

    add:multer_1

--- server.js
+++ server.js
@@ -2,104 +2,85 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const models = require("./models");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 const port = 8080;
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/products", (req, res) => {
-  const query = req.query;
-  console.log("QUERY : ", query);
-  res.send({
-    products: [
-      {
-        id: 1,
-        name: "NFT마켓",
-        price: 5,
-        seller: "박성범",
-        imageUrl: "images/products/nftmarket.png",
-      },
-      {
-        id: 2,
-        name: "토이 프로젝트",
-        price: 5,
-        seller: "김성훈",
-        imageUrl: "images/products/toyproject.png",
-      },
-      {
-        id: 3,
-        name: "미니 프로젝트",
-        price: 5,
-        seller: "류재준",
-        imageUrl: "images/products/nftmarket.png",
-      },
-      {
-        id: 4,
-        name: "미니 프로젝트",
-        price: 5,
-        seller: "류재준",
-        imageUrl: "images/products/nftmarket.png",
-      },
-      {
-        id: 5,
-        name: "미니 프로젝트",
-        price: 5,
-        seller: "류재준",
-        imageUrl: "images/products/nftmarket.png",
-      },
-      {
-        id: 6,
-        name: "토이 프로젝트",
-        price: 5,
-        seller: "김성훈",
-        imageUrl: "images/products/toyproject.png",
-      },
-      {
-        id: 7,
-        name: "토이 프로젝트",
-        price: 5,
-        seller: "김성훈",
-        imageUrl: "images/products/toyproject.png",
-      },
-      {
-        id: 8,
-        name: "토이 프로젝트",
-        price: 5,
-        seller: "김성훈",
-        imageUrl: "images/products/toyproject.png",
-      },
-      {
-        id: 9,
-        name: "토이 프로젝트",
-        price: 5,
-        seller: "김성훈",
-        imageUrl: "images/products/toyproject.png",
-      },
-      {
-        id: 10,
-        name: "토이 프로젝트",
-        price: 5,
-        seller: "김성훈",
-        imageUrl: "images/products/toyproject.png",
-      },
-    ],
-  });
+  models.Product.findAll({
+    limit: 10,
+    order: [["createdAt", "DESC"]],
+    attributes: ["id", "name", "price", "createdAt", "seller", "imageUrl"],
+  })
+    .then((result) => {
+      console.log("PRODUCTS : ", result);
+      res.send({ products: result });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("에러 발생");
+    });
 });
 
 app.post("/products", (req, res) => {
   const body = req.body;
-  res.send({
-    // body: body
-    body,
-  });
+  const { name, description, price, seller } = body;
+  if (!name || !description || !price || !seller) {
+    res.send("모든 필드를 입력해주세요");
+  }
+  models.Product.create({
+    name,
+    description,
+    price,
+    seller,
+  })
+    .then((result) => {
+      console.log("상품 생성 결과 : ", result);
+      res.send({
+        result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("상품 업로드에 문제가 발생했습니다");
+    });
 });
 
-app.get("/products/:id/events/:eventId", (req, res) => {
+app.get("/products/:id", (req, res) => {
   const params = req.params;
-  const { id, eventId } = params;
-  res.send(`id는 ${id}와 ${eventId}입니다.`);
+  const { id } = params;
+  models.Product.findOne({
+    where: {
+      id: id,
+    },
+  })
+    .then((result) => {
+      console.log("PRODUCT : ", result);
+      res.send({
+        product: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("상품 조회에 에러가 발생했습니다");
+    });
 });
+
+app.post("/image", upload.single());
 
 app.listen(port, () => {
   console.log("범마켓 서버가 돌아가고 있습니다");
+  models.sequelize
+    .sync()
+    .then(() => {
+      console.log("DB 연결 성공!");
+    })
+    .catch((err) => {
+      console.error(err);
+      console.log("DB 연결 에러ㅠ");
+      process.exit();
+    });
 });
